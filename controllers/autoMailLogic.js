ac8f26f9a38c492c19bb687a025031255c416d2f
siddhesh1051 <siddhesh1052001@gmail.com>
2023-11-10 13:39:32 +0530

    controller setup

---
+++ controllers/autoMailLogic.js
@@ -0,0 +1,5 @@
+const autoMailLogic = async (req, res) => {
+    
+}
+
+module.exports = { autoMailLogic };
--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const path = require("path");
+const { autoMailLogic } = require("./controllers/autoMailLogic");
 const port = 4000;
-
 const app = express();
+
+
+const botFolder = "botMails";
+const SCOPES = [
+    "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/gmail.labels",
+    "https://mail.google.com/",
+  ];
+
+
+app.get("/", autoMailLogic);
 
 
 app.listen(port, () => {
